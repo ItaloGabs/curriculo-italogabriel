77e1cc64e7cde98f0a0c1939498509b749dab822
Itaru DELL <gabrielgg.tavares@gmail.com>
2022-02-28 16:47:41 -0300

    prototype-1.5

---
+++ Javascript/barras.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $('#barra').click(function () {
+        $('#dev-front').slideToggle('slow');
+    })
+    $('#barra1').click(function () {
+        $('#github').slideToggle('slow');
+    })
+})
+
+
+
+
+
+
+
